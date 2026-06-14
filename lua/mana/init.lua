714d018dee59ef2e20d1b6d09c337ba75d30bb7d
Abdulqadir Ahmad <arfs6.mail@gmail.com>
2026-06-14 10:12:09 +0100

    refactor: moved autocmd in init.lua to a new file triggers.lua.
    Also, removed keymap in init.lua. Will add it back later.

--- lua/mana/init.lua
+++ lua/mana/init.lua
@@ -3,55 +3,12 @@
 --]]
 local m = {}
 
-local api = vim.api
-local fn = vim.fn
-
 local log = require('plenary.log').new({
 	plugin = 'mana',
 	level = 'debug',
 })
 
-local keys = require('mana.keys')
-
-local espeak_ng = require('mana.espeak-ng')
-
-vim.on_key(
-	function(key, typed)
-		local text = keys[vim.fn.keytrans(typed)] or vim.fn.keytrans(typed)
-		espeak_ng.stop()
-		espeak_ng.speak(text)
-	end,
-	0,
-	{}
-)
-
-vim.api.nvim_create_autocmd('CursorMoved', {
-	callback = function()
-		log.info('Cursor moved.')
-		local pos = vim.fn.getcurpos()
-		local cursorPos = { pos[2], pos[3] }
-		local line = api.nvim_get_current_line()
-		local text = ''
-		if m.cursorPos and cursorPos[1] == m.cursorPos[1] then
-			if m.cursorPos[2] > cursorPos[2] then
-				text = string.sub(line, cursorPos[2], m.cursorPos[2])
-				if #text == 2 then
-					text = string.sub(text, 1, 1)
-				end
-			else
-				text = string.sub(line, m.cursorPos[2], cursorPos[2])
-				if #text == 2 then
-					text = string.sub(text, 2, 2)
-				end
-			end
-		else
-			text = line
-		end
-		espeak_ng.speak(text)
-		m.cursorPos = cursorPos
-	end
-})
-
-vim.keymap.set('n', '<a-s>', function() espeak_ng.stop() end)
+local triggers = require('mana.triggers')
+triggers.init()
 
 return m
---
+++ lua/mana/triggers.lua
@@ -0,0 +1,60 @@
+--[[
+--This module contains the part of mana.nvim that speaks automatically
+--]]
+--
+local m = {}
+
+local api = vim.api
+
+local log = require('plenary.log').new({
+	plugin = 'mana',
+	level = 'debug',
+})
+
+local keys = require('mana.keys')
+local tts = require('mana.tts')
+
+m.on_key = function(key, typed)
+	local text = keys[vim.fn.keytrans(typed)] or vim.fn.keytrans(typed)
+	tts.stop()
+	tts.speak(text)
+end
+
+m.events = {
+	CursorMoved = {
+		callback = function()
+			log.info('Cursor moved.')
+			local pos = vim.fn.getcurpos()
+			local cursorPos = { pos[2], pos[3] }
+			local line = api.nvim_get_current_line()
+			local text = ''
+			if m.cursorPos and cursorPos[1] == m.cursorPos[1] then
+				if m.cursorPos[2] > cursorPos[2] then
+					text = string.sub(line, cursorPos[2], m.cursorPos[2])
+					if #text == 2 then
+						text = string.sub(text, 1, 1)
+					end
+				else
+					text = string.sub(line, m.cursorPos[2], cursorPos[2])
+					if #text == 2 then
+						text = string.sub(text, 2, 2)
+					end
+				end
+			else
+				text = line
+			end
+			tts.speak(text)
+			m.cursorPos = cursorPos
+		end
+	}
+}
+
+m.init = function()
+	vim.on_key(m.on_key, 0, {})
+
+	for event, opts in pairs(m.events) do
+		api.nvim_create_autocmd(event, opts)
+	end
+end
+
+return m
